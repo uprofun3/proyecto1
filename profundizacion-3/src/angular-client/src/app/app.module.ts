65b40125f7a9eb94b03ed31056f363e0c8a6f9dc
Daividix <david_acss@hotmail.com>
2018-08-31 14:02:24 -0500

    componentes de angular

--- profundizacion-3/src/angular-client/src/app/app.module.ts
+++ profundizacion-3/src/angular-client/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SiteComponent } from './components/site/site.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfileComponent,
+    SiteComponent,
+    CategoriesComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
