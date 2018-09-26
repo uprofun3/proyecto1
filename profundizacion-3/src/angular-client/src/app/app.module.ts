7566296558133fd09ae3544699ed9bcf612b4b58
Daividix <david_acss@hotmail.com>
2018-09-26 14:23:09 -0500

    agregados modulos para hecer peticiones http, modulos para tratar formularios

--- profundizacion-3/src/angular-client/src/app/app.module.ts
+++ profundizacion-3/src/angular-client/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+
+import { RegisterService } from './services/register.service'
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -10,6 +14,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { SiteComponent } from './components/site/site.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { SearchComponent } from './components/search/search.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +26,15 @@ import { SearchComponent } from './components/search/search.component';
     ProfileComponent,
     SiteComponent,
     CategoriesComponent,
-    SearchComponent
+    SearchComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RegisterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
